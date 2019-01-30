16b01d0787bc331bc6434617fb6b6f4b1ab072a9
kraiwich <le_kraiwich@hotmail.com>
2019-01-30 12:30:29 +0700

    initalize project files

---
+++ mobile_game2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mobile_game2
+//
+//  Created by student on 30/1/2562 BE.
+//  Copyright © 2562 jeff.inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
